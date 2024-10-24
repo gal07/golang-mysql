e1f9940c09dafd9866438dd6fe2caebe29dbe526
Galih Kurnia <galihkur29@gmail.com>
2024-10-25 02:07:11 +0700

    betulin pencegahan insert lesson dan student yang sudah ada.

--- modules/report/delivery/rest/insert_report.go
+++ modules/report/delivery/rest/insert_report.go
@@ -23,8 +23,8 @@ func (e endpoint) InsertReport(c *gin.Context) {
 	}
 
 	// Check Every Student Have Grade Each Lesson if total of lesson is 5 then student must have report each lesson which is 5)
-	find, err := e.useCaseReport.GetReportByStudentnLesson(c, payload.ReqGetByStudentLesson{StudentID: payloads.Student, LessonID: payloads.Lesson})
-	if !find {
+	found, err := e.useCaseReport.GetReportByStudentnLesson(c, payload.ReqGetByStudentLesson{StudentID: payloads.Student, LessonID: payloads.Lesson})
+	if found {
 		util.ResponseErrorCustom(c, 200, nil, "Can't entering same student & lesson")
 		panic(err)
 	}
