0ff3e60a651c6110f4ec451e3b22a2ea2a4d9feb
Galih Kurnia <galihkur29@gmail.com>
2024-11-05 00:41:59 +0700

    prevent before update, because after exec update no result to know is it success or fail

--- modules/report/usecase/update_report.go
+++ modules/report/usecase/update_report.go
@@ -2,11 +2,22 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"gosql/modules/report/models"
 	"gosql/modules/report/payload"
 )
 
 func (s *reportUseCase) UpdateReport(ctx context.Context, req models.Report) (res payload.ResUpdate, err error) {
+
+	// matching id ,student and lesson
+	get, err := s.reportEntity.ReportRepo.GetDetail(ctx, payload.ReqGetDetail{ID: req.ID})
+	if err != nil {
+		return res, err
+	}
+
+	if get.Lesson != req.Lesson || get.Student != req.Student || get.ID != req.ID {
+		return res, errors.New("request data is not match")
+	}
 
 	// Update
 	if _, err := s.reportEntity.ReportRepo.Update(ctx, req); err != nil {
