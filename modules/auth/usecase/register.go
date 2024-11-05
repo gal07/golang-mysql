3cf2f59aa8e248ed4765bede2a86d8ccb6ba1d94
Galih Kurnia <galihkur29@gmail.com>
2024-11-05 23:11:11 +0700

    add email sender

--- modules/auth/usecase/register.go
+++ modules/auth/usecase/register.go
@@ -3,20 +3,36 @@ package usecase
 import (
 	"context"
 	"gosql/modules/auth/models"
+	"gosql/modules/auth/payload"
+	util "gosql/utility"
+	"runtime"
 )
 
-func (s *authUseCase) Register(ctx context.Context, req models.Auth) (res models.Auth, err error) {
+func (s *authUseCase) Register(ctx context.Context, req models.Auth) (res payload.ResRegister, err error) {
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// Encrypt password
 	req.EncryptPassword()
 
 	// Insert
-	register, err := s.authEntity.AuthRepo.Insert(ctx, req)
+	_, err = s.authEntity.AuthRepo.Insert(ctx, req)
 	if err != nil {
 		return res, err
 	}
 
-	res = register
+	// send email
+	var mails = []string{}
+	var cc = []string{}
+	mails = append(mails, req.Email)
+	cc = append(cc, req.Email)
+	go util.Send(mails, cc)
+
+	// Fill payload
+	res.Email = req.Email
+	res.Username = req.Username
+	res.Is_active = false
+	res.Is_registered = true
 
 	return res, nil
 
---
+++ utility/mail.go
@@ -0,0 +1,50 @@
+package utility
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+	"strings"
+)
+
+const CONFIG_SMTP_HOST = "smtp.gmail.com"
+const CONFIG_SMTP_PORT = 587
+const CONFIG_SENDER_NAME = "Read Me Boy <galihkur29@gmail.com>"
+const CONFIG_AUTH_EMAIL = "galihkur29@gmail.com"
+const CONFIG_AUTH_PASSWORD = "aqogepbxmthlljpl"
+
+func Send(target []string, target_cc []string) {
+
+	to := target
+	cc := target_cc
+	subject := "Test mail"
+	message := "Hello"
+
+	err := sendMail(to, cc, subject, message)
+	if err != nil {
+		CreateLog(err)
+		// log.Fatal(err.Error())
+	}
+
+	log.Println("Mail sent!")
+
+}
+
+func sendMail(to []string, cc []string, subject, message string) error {
+	body := "From: " + CONFIG_SENDER_NAME + "\n" +
+		"To: " + strings.Join(to, ",") + "\n" +
+		"Cc: " + strings.Join(cc, ",") + "\n" +
+		"Subject: " + subject + "\n\n" +
+		message
+
+	auth := smtp.PlainAuth("", CONFIG_AUTH_EMAIL, CONFIG_AUTH_PASSWORD, CONFIG_SMTP_HOST)
+	smtpAddr := fmt.Sprintf("%s:%d", CONFIG_SMTP_HOST, CONFIG_SMTP_PORT)
+
+	err := smtp.SendMail(smtpAddr, auth, CONFIG_AUTH_EMAIL, append(to, cc...), []byte(body))
+	if err != nil {
+		CreateLog(err)
+		return err
+	}
+
+	return nil
+}
